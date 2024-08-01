d89566e2cf45a8ce7180bdb43328190c432a6336
nikolayv <kvadeck@gmail.com>
2024-08-01 20:09:40 +0300

    Add login/register tabs and register validation form

--- vue-music-pinia/src/main.js
+++ vue-music-pinia/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createPinia} from "pinia";
 import App from './App.vue'
 import './assets/styles/base-compiled.css'
 import './assets/styles/main.css'
+import VeeValidatePlugin from './includes/validation'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(createPinia());
+app.use(VeeValidatePlugin)
+app.mount('#app')
